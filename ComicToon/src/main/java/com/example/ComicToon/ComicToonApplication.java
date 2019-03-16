dcaa35f8d86e698e5d2456f48c90796df6af5046
Qi Yuan Fang <qiyuan.fang@stonybrook.edu>
2019-03-16 15:26:38 -0400

    Spring project skeleton, generated by Spring init -> web, REST, MongoDB plugin JAVA 11 REQUIRED

---
+++ ComicToon/src/main/java/com/example/ComicToon/ComicToonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ComicToon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicToonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicToonApplication.class, args);
+	}
+
+}
