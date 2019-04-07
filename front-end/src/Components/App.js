30215467a01d1580cce0cf281373ab85ccca4a4a
Joel George <joelgeorge556@gmail.com>
2019-04-07 18:15:18 -0400

    moved redux provider

--- front-end/src/Components/App.js
+++ front-end/src/Components/App.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
 import { Image } from 'react-bootstrap'; 
 import './styles/App.css';
-import {Provider} from 'react-redux'
+// import {Provider} from 'react-redux'
 import Authentication from './Authentication';
 import HomeContent from './HomeContent';
 import logo from './images/logo.jpg';
 
 import Footer from './Footer';
-import store from './Store'
+// import store from './Store'
 class App extends Component {
 
 	render() {
 		if (!window.localStorage.getItem('user')) {
-			return (
-				<Provider store={store}>
-					<div className="App-unauth-container">
-						<div className="App-unauth">
-							<div className = "logo">
-								<Image className="logoimage" src={logo} fluid width="200" />
-							</div>
-							<Authentication />
-							<Footer/>
+			return (				
+				<div className="App-unauth-container">
+					<div className="App-unauth">
+						<div className = "logo">
+							<Image className="logoimage" src={logo} fluid width="200" />
 						</div>
+						<Authentication />
+						<Footer/>
 					</div>
-				</Provider>
+				</div>
 			);
 		} else {
 			return (
-				<Provider store={store}>
-					<HomeContent />
-				</Provider>
+				<HomeContent />
 			);
 		}
 	}
