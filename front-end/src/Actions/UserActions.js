9eac78c440a04fc470333408a02e342ae8352e72
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-04-24 21:27:46 -0400

    Receive token in login

--- front-end/src/Actions/UserActions.js
+++ front-end/src/Actions/UserActions.js
@@ -19,19 +19,19 @@ export const LoginUser = (email, pwd) => (dispatch) => {
                 alert("INCORRECT EMAIL OR PASSWORD!!");
                 dispatch({
                     type: ERR,
-                    payload: {username: "", id: "", active: "", pwd: "", email: ""}
+                    payload: {username: "", id: "", active: "", token: "", email: ""}
                 });
             }  else if (content.status === "User is not verified!") {
                 dispatch({
                     type: LOGIN_USER,
-                    payload: {username: "", id: "", active: "", pwd: "", email: "", verified: false}
+                    payload: {username: "", id: "", active: "", token: "", email: "", verified: false}
                 });
             }
             else { 
                 console.log(content);
                 dispatch({
                     type: LOGIN_USER,
-                    payload: {username: content.username, id: content.id, active: content.active, pwd: pwd, email: email, verified: true}
+                    payload: {username: content.username, id: content.id, active: content.active, token: content.token, email: email, verified: true}
                 });
             }
     })();
