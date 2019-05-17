df0a42d53903016f0ed7e9b71a92b7c34aa25a02
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-05-17 15:31:40 -0400

    Fixed some CSS create comic

--- front-end/src/Components/CreateComic.js
+++ front-end/src/Components/CreateComic.js
@@ -234,7 +234,7 @@ class CreateComic extends Component {
                 {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: false
--- front-end/src/Components/Panel.js
+++ front-end/src/Components/Panel.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Panel extends Component {
     render() {
-        return this.props.comic ? <img style={{width: "100%", border: "1px solid black"}} src={this.props.comic.image} alt="Click to edit and display comic" /> : null;
+        return this.props.comic ? <img className="panel" style={{width: "100%", border: "1px solid black"}} src={this.props.comic.image} alt="Click to edit and display comic" /> : null;
     }
 }
 
