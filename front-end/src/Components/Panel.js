55034311ed67351a8f60df96152aacfe304943ec
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-05-21 15:02:13 -0400

    Create comic edit panel, fixed bug in shared comics

--- front-end/src/Components/Panel.js
+++ front-end/src/Components/Panel.js
@@ -4,7 +4,7 @@ class Panel extends Component {
     render() {
         return this.props.comic ? 
             <div style={{ display: "inline-block", position: "relative" }} onDragOver={this.props.dragover} >
-                <img className="panel" style={{width: "100%", border: "1px solid black"}} src={this.props.comic.image} alt="Could not display comic" onDragStart={this.props.dragstart} onDragEnd={this.props.dragend} />
+                <img className="panel" style={{width: "100%", border: "1px solid black"}} onClick={this.props.edit} src={this.props.comic.image} alt="Could not display comic" onDragStart={this.props.dragstart} onDragEnd={this.props.dragend} />
                 <span className="close-panel-icon" onClick={this.props.close}>✕</span>
             </div> : null;
     }
